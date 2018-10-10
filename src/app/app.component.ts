6904a06d7cdcab70478854380a5bb712760ae325
Rémi Messembourg <remi.messembourg@axiocode.com>
2018-10-10 17:15:58 +0200

    Added Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Post} from './type/type';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-blog';
+  posts: Array<Post> = [
+    {
+      title: 'premier post',
+      content:  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc malesuada eleifend consectetur. Aenean et nibh felis. Sed ' +
+                'aliquet purus ac neque porta, a rutrum felis pellentesque. Proin euismod justo ut elit malesuada scelerisque. Nulla ' +
+                'condimentum, justo et tempus molestie, neque sapien cursus dolor, non rhoncus nisi mi sit amet risus. Proin facilisis ' +
+                'urna sit amet fermentum sodales. Suspendisse potenti. Nulla vestibulum nunc magna, non semper odio imperdiet ut.' +
+                ' Vivamus cursus sapien vel molestie iaculis. Sed vel vehicula magna, condimentum lobortis erat. Curabitur ac libero ' +
+                'commodo massa mollis tincidunt at vel diam.',
+      loveIts: 0,
+      created_at : new Date('2018-10-08')
+    },
+    {
+      title: 'SECOND post',
+      content:  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc malesuada eleifend consectetur. Aenean et nibh felis. Sed ' +
+      'aliquet purus ac neque porta, a rutrum felis pellentesque. Proin euismod justo ut elit malesuada scelerisque. Nulla ' +
+      'condimentum, justo et tempus molestie, neque sapien cursus dolor, non rhoncus nisi mi sit amet risus. Proin facilisis ' +
+      'urna sit amet fermentum sodales. Suspendisse potenti. Nulla vestibulum nunc magna, non semper odio imperdiet ut.' +
+      ' Vivamus cursus sapien vel molestie iaculis. Sed vel vehicula magna, condimentum lobortis erat. Curabitur ac libero ' +
+      'commodo massa mollis tincidunt at vel diam.',
+      loveIts: 0,
+      created_at : new Date('2018-10-09')
+    },
+    {
+      title: 'DERNIER POST',
+      content:  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc malesuada eleifend consectetur. Aenean et nibh felis. Sed ' +
+      'aliquet purus ac neque porta, a rutrum felis pellentesque. Proin euismod justo ut elit malesuada scelerisque. Nulla ' +
+      'condimentum, justo et tempus molestie, neque sapien cursus dolor, non rhoncus nisi mi sit amet risus. Proin facilisis ' +
+      'urna sit amet fermentum sodales. Suspendisse potenti. Nulla vestibulum nunc magna, non semper odio imperdiet ut.' +
+      ' Vivamus cursus sapien vel molestie iaculis. Sed vel vehicula magna, condimentum lobortis erat. Curabitur ac libero ' +
+      'commodo massa mollis tincidunt at vel diam.',
+      loveIts: 0,
+      created_at : new Date()
+    },
+  ];
 }
